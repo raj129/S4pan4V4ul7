1685e2eed71e6cc0b4b7e4ed2291efb8faa1aede
Nimbalkar, Ajinkya  AE/PUN-RM521 <nimbaain@schaeffler.com>
2026-09-20 12:56:39 +0530

    feat: refactor main function for improved Firebase initialization and error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
-import 'dart:async';
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/widgets.dart';
-import 'presentation/app/vault_app.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
 import 'firebase_options.dart';
+import 'presentation/app/vault_app.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await runZonedGuarded<Future<void>>(() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-    PlatformDispatcher.instance.onError = (error, stack) {
-      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      return true;
-    };
-    await GoogleSignIn.instance.initialize(
-      serverClientId: '209716874258-p9n2n9jmu87oqqu84703hf9kvuodokdn.apps.googleusercontent.com',
-    );
-    runApp(const VaultApp());
-  }, (error, stack) {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-  });
+    return true;
+  };
+
+  await GoogleSignIn.instance.initialize(
+    serverClientId:
+        '209716874258-p9n2n9jmu87oqqu84703hf9kvuodokdn.apps.googleusercontent.com',
+  );
+
+  runApp(const VaultApp());
 }
